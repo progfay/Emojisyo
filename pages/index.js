482279e3e283cb2310129e255fce14600efa3f4c
progfay <shun1856shun@gmail.com>
2018-12-27 22:17:35 +0900

    feat: form the frame of list-page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,41 @@
-
+import React from 'react'
 import Head from '../components/head'
 
 export default () => (
-  <>
+  <React.Fragment>
     <Head />
-    <h1>
-    Hello, world
-    </h1>
-  </>
+    <div className='container'>
+      <input type='text' />
+      <div className='item-list'>
+        <div className='item' />
+        <div className='item' />
+        <div className='item' />
+        <div className='item' />
+        <div className='item' />
+      </div>
+    </div>
+    <style jsx>{`
+      .container {
+        width: 100vw;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+      }
+
+      .item-list {
+        width: 80vw;
+        border: solid 1px black;
+      }
+
+      .item {
+        width: 100%;
+        height: 80px;
+      }
+
+      .item:not(:last-child) {
+        border-bottom: solid 1px gray;
+      }
+    `}</style>
+  </React.Fragment>
 )
