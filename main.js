260dea3a495f77f2afeab8ee5763dc57bdf8bacc
progfay <shun1856shun@gmail.com>
2018-12-29 01:05:05 +0900

    refactor: remove unused code

--- main.js
+++ main.js
@@ -2,13 +2,11 @@ const { send } = require('micro')
 const { router, get } = require('microrouter')
 const handler = require('serve-handler')
 
-const hoge = (req, res) => send(res, 200, 'hoge')
 const searchList = (req, res) => send(res, 200, { 'items': [{ 'query': '', 'results': ['Montgomery, Alabama', 'Juneau, Alaska', 'Phoenix, Arizona', 'Little Rock, Arkansas'] }] })
 const notfound = (req, res) => send(res, 404, 'Not found route')
 
 module.exports = router(
   get('/', handler),
-  get('/hoge', hoge),
   get('/search_list', searchList),
   get('/*', notfound)
 )
