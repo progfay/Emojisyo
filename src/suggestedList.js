52b7aa9458c5febc5d663a0dc18590675481277a
progfay <shun1856shun@gmail.com>
2018-12-29 17:39:37 +0900

    feat: impl asearch system

--- src/suggestedList.js
+++ src/suggestedList.js
@@ -1,11 +1,21 @@
 const { send } = require('micro')
+const Asearch = require('asearch')
 const emojiMap = require('./emojiMap')
 
 const suggested = (input) => {
-  if (!input) {
-    return []
+  if (!input) return []
+
+  let emojiList = []
+  const asearch = new Asearch(` ${input} `)
+  for (const ambiguity of [0, 1, 2]) {
+    for (const keyword of Object.keys(emojiMap)) {
+      if (!asearch.match(keyword, ambiguity)) continue
+      emojiList = emojiList.concat(emojiMap[keyword])
+    }
+    if (emojiList.length > 0) break
   }
-  return emojiMap[input] || []
+
+  return Array.from(new Set(emojiList))
 }
 
 module.exports = ({ query: { input = '' } = {} }, res) => (
